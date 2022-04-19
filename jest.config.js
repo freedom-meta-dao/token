2b17603584faac86ab77a355605884d05edaee9d
Michael Brich <mike@toreda.com>
2022-04-19 12:37:34 -0700

    Added additional folders to VSCode file exclusions. Replaced use of @babel/jest and ts-jest in jest config with @swc/jest. Updated package dependencies to relevant latest versions

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-	testEnvironment: "jest-environment-node-single-context",
+	testEnvironment: 'jest-environment-node-single-context',
 	transform: {'^.+\\.tsx?$': 'ts-jest'},
 	testRegex: '(/__test__/.*|(\\.|/)(spec))\\.ts$',
 	roots: ['./'],
 	coverageDirectory: './coverage',
+	testResultsProcessor: 'jest-sonar-reporter',
 	coveragePathIgnorePatterns: [
-		'tests/',
+		'test/',
 		'node_modules/',
 		'.node/',
 		'jest/',
 		'coverage/',
 		'webpack.config.js'
-	],
+	]
 };
