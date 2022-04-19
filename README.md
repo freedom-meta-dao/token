3bf41090b610a218ac40424b7d6fab7f1e5f3dfe
Michael Brich <mike@toreda.com>
2022-04-19 13:29:08 -0700

    Updated dependencies. Removed unused packages. Added additional config files to .npmignore and .gitignore.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Freedom Token
+# Freedom MetaDAO Token
 
 [![CI](https://img.shields.io/github/workflow/status/freedom-meta-dao/token/CI?style=for-the-badge)](https://github.com/freedom-meta-dao/token/actions)
 [![Coverage](https://img.shields.io/sonar/coverage/freedom-meta-dao_token?server=https%3A%2F%2Fsonarcloud.io&style=for-the-badge)](https://sonarcloud.io/dashboard?id=freedom-meta-dao_token)
@@ -9,3 +9,4 @@
 [![GitHub issues](https://img.shields.io/github/issues/freedom-meta-dao/token?style=for-the-badge)](https://github.com/freedom-meta-dao/token/issues)
 
  [![license](https://img.shields.io/github/license/freedom-meta-dao/token?style=for-the-badge)](https://github.com/freedom-meta-dao/token/blob/master/LICENSE)
+
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,67 +1,80 @@
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-ethers";
-import "@typechain/ethers-v5";
+import "@typechain/hardhat";
 import "hardhat-gas-reporter";
-import 'hardhat-jest-plugin';
-import "tsconfig-paths/register";
-import "typechain";
+import "hardhat-jest-plugin";
 
-import { HardhatUserConfig, task } from "hardhat/config";
+import "solidity-coverage";
+import "@nomiclabs/hardhat-solpp";
+
+import {HardhatUserConfig, task} from "hardhat/config";
 
 import {config as dotEnvConfig} from "dotenv";
-
-require("solidity-coverage");
 
 dotEnvConfig();
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+	const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+	for (const account of accounts) {
+		console.log(account.address);
+	}
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
+/**
+ * Token build config.
+ */
 const config: HardhatUserConfig = {
-  "defaultNetwork": "hardhat",
-  paths: {
-    artifacts: "./dist/artifacts",
-    tests: "./tests",
-    root: ".",
-    sources: "./contracts",
-  },
-  solidity: {
-    compilers: [
-    {version: "0.8.4",
-    settings: {}}]
-  },
-  networks: {
-    ropsten: {
-      chainId: 3,
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    bsctest: {
-      chainId: 97,
-      url: process.env.BSCTEST_URL || "",
-      timeout: 1000,
+	defaultNetwork: "hardhat",
+	paths: {
+		artifacts: "dist/artifacts",
+		tests: "tests",
+		root: ".",
+		sources: "contracts",
+		cache: ".cache"
+	},
+	typechain: {
+		outDir: "typechain/types",
+		target: "ethers-v5",
+		alwaysGenerateOverloads: false,
+		externalArtifacts: ["./dist/artifacts/**.json"]
+	},
+	solidity: {
+		compilers: [
+			{
+				version: "0.8.4",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 50
+					}
+				}
+			}
+		]
+	},
+	networks: {
+		ropsten: {
+			chainId: 3,
+			url: process.env.ROPSTEN_URL || "",
+			accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+		},
+		bsctest: {
+			chainId: 97,
+			url: process.env.BSCTEST_URL || "",
+			timeout: 1000
+		},
+		hardhat: {
 
-    },
-    hardhat: {},
-    localhost: {}
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+		},
+		localhost: {}
+	},
+	gasReporter: {
+		enabled: process.env.REPORT_GAS !== undefined,
+		currency: "USD"
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_API_KEY
+	}
 };
 
 export default config;
